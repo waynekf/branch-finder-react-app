aca2a5c25ebc1a0d6c94d2a678a1e3370f05e9bd
Wayne King-Farlow <wayne.king-farlow@skipton.co.uk>
2025-09-30 10:01:30 +0100

    Remove obsolete code

--- branch-finder-react-app/src/app/data/branches.ts
+++ branch-finder-react-app/src/app/data/branches.ts
@@ -3,38 +3,6 @@ import { Branch } from '../schema/branch';
 function getBranches(): Branch[] {
   return [
     {
-      name: 'Bracknell',
-      address: {
-        addressLine1: 'somewhere in bracknell',
-        addressLine2: '',
-        town: 'Bracknell',
-        county: 'Berkshire',
-        postcode: 'RG423SH',
-      },
-      coordinates: {
-        latitude: 51.423723,
-        longitude: -0.739431,
-        eastings: 487742,
-        northings: 170166,
-      },
-    },
-    {
-      name: 'Slough',
-      address: {
-        addressLine1: 'somewhere in Slough',
-        addressLine2: '',
-        town: 'Slough',
-        county: 'Berkshire',
-        postcode: 'SL24HL',
-      },
-      coordinates: {
-        latitude: 51.532009,
-        longitude: -0.573974,
-        eastings: 499011,
-        northings: 182419,
-      },
-    },
-    {
       name: 'Reading',
       address: {
         addressLine1: '30 Queen Victoria Street',
